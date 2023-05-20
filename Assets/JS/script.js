52f93c2f7e6cbc671a3b2a42b55b1e9238661f19
FCarter09 <fc_vstate@yahoo.com>
2023-05-20 12:56:55 -0400

    timer-score update

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,20 +2,44 @@
 
 
 var beginQuizEl = document.querySelector("#start-quiz");
-var timer = 60;
-var userScore = 0;
-var userPoints = 20;
+var time = 60;
 
-
+ 
+// start quiz on intro page
 var startQuiz = function(){
+
+    
 
     var beginQuizEl = document.querySelector("#start-quiz");
     alert("The quiz has begun, timer has started at 60 seconds!");
+    timer();
     questionOne();
 
 
 }
 
+// add event listener to start quiz button
+beginQuizEl.addEventListener("click", startQuiz);
+
+// timer function
+var timer = function() {
+
+    var timeRemaining = document.querySelector("#timeRemaining")
+
+     // starts timer countdown starting from value of timeLeft
+    var countDown = setInterval(function() {
+        time--;
+        timeRemaining.textContent = time;
+        console.log(time);
+
+        if (time <= 0) {
+            clearInterval(countDown);   // stops at 0
+            // timerElement.textContent = "Make final choice and game will end!";
+        }
+    }, 1000);    //counts in milliseconds. 1000ms = 1second
+}
+
+// question one function
 var questionOne = function(){
 
     //delete intro screen
@@ -58,12 +82,16 @@ var questionOne = function(){
         
         alert("Incorrect!");
 
+        time -= 20
+
         questionTwo();
     })
 
     answerChoiceTwo.addEventListener('click', function(event){
 
         alert("Incorrect!");
+
+        time -= 20
 
         questionTwo();
     })
@@ -72,15 +100,19 @@ var questionOne = function(){
 
         alert("Incorrect!");
 
+        time -= 20
+
 
         questionTwo();
     })
 
     answerChoiceFour.addEventListener('click', function(event) {
+        
+        time += 20
 
-        userScore = userScore;
-        console.log(userScore + " is now " + userPoints + ".");
-        alert("Correct! You've earned 20 points");
+        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+
+        console.log(" User Score is now " + time + ".");
 
         questionTwo();
     })
@@ -89,8 +121,10 @@ var questionOne = function(){
 
 }
 
-
+//question two function
 var questionTwo = function() {
+
+    // delete question one
 
   //delete questionOneEl
   var questionOneEl = document.querySelector("div");
@@ -149,6 +183,8 @@ var questionTwo = function() {
     answerChoiceOne.addEventListener('click', function(event) {
        
         alert("Incorrect!");
+
+        time -= 20
         
 
         questionThree();
@@ -158,18 +194,20 @@ var questionTwo = function() {
 
         alert("Incorrect!");
 
+        time -= 20
+
 
         questionThree();
 
     })
 
     answerChoiceThree.addEventListener('click', function(event){
-
-        userScore = userPoints 
-        console.log(userScore + " is now " + userPoints + ".");
-        alert("Correct! You've earned 20 points");
         
+        time += 20
 
+        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+
+        console.log( " User Score is now " + time + ".");
 
         questionThree();
 
@@ -179,11 +217,14 @@ var questionTwo = function() {
         
         alert("Incorrect!");
 
+        time -= 20
+
         questionThree();
 })
 
 }
 
+// question three function
 var questionThree = function(){
 
     var questionTwoEl = document.querySelector("div");
@@ -242,13 +283,21 @@ var questionThree = function(){
 
         alert("Incorrect!");
 
+        time -= 20
+
+        conclusionPage();
+
     })
 
     answerChoiceTwo.addEventListener('click', function(event){
+        
+        time += 20
 
-        userScore = userPoints
-        console.log(userScore + " is now " + userPoints + ".");
-        alert("Correct! You've earned 20 points");
+        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+
+        console.log("User score is now " + time + ".");
+        
+        conclusionPage();
 
 
     })
@@ -257,16 +306,93 @@ var questionThree = function(){
 
         alert("Incorrect!");
 
+        time -= 20
+        
+        conclusionPage();
+
     })
 
     answerChoiceFour.addEventListener('click', function(event){
 
         alert("Incorrect!");
 
+        time -= 20
+       
+        conclusionPage();
+
+
+    })
+
+}
+
+// conclusion function
+var conclusionPage = function() {
+
+    var finalScore = time
+
+    var questionThreeEl = document.querySelector("div");
+    questionThreeEl.remove();
+    console.log("removing questionTwoEl");
+
+    //remove answer buttons for questionThreeEl
+    var answerChoiceOne = document.querySelector("button")
+    answerChoiceOne.remove();
+    console.log("removing answerChoiceOne");
+
+    var answerChoiceTwo = document.querySelector("button")
+    answerChoiceTwo.remove();
+    console.log("removing answerChoiceTwo");  
+
+    var answerChoiceThree = document.querySelector("button")
+    answerChoiceThree.remove();
+    console.log("removing answerChoiceThree"); 
+
+    var answerChoiceFour = document.querySelector("button")
+    answerChoiceFour.remove();
+    console.log("removing answerChoiceFour"); 
+
+     //selecting body tag
+     var bodyTag = document.querySelector("body")
+
+     var conclusion = document.createElement("h1");
+    conclusion.className = "conclusion";
+    conclusion.textContent = "All Done!";
+    bodyTag.appendChild(conclusion);
+
+    var message = document.createElement("p");
+    message.className = "message";
+    message.textContent = "Your final score is: " + finalScore;
+    bodyTag.appendChild(message);
+
+    var initials = document.createElement("p")
+    initials.className = "initials"
+    initials.textContent = "Enter initials:"
+    bodyTag.appendChild(initials)
+
+    var input = document.createElement("input")
+    input.className = "initialsInput"
+    bodyTag.appendChild(input)
+
+    var submitButton = document.createElement("button")
+    submitButton.className = "submit-score-button"
+    submitButton.textContent = "Submit"
+    bodyTag.appendChild(submitButton)
+
+    submitButton.addEventListener("click", function() {
+    // should save final score + initials in local storage. Look at examples from weather App to do this
+    // get data from local storage
+    // var highScores = JSON.parse(localStorage.getItem("highScores") || "[]")
+
+    console.log('This is working');
+
+    // add current data to local storage
+    // example of what data should look like this side is text content of input box -> {"EP": 100} <- this side is the finalScore
+    //                                                                                          currentPlayer: finalScore
+    
+    // set into local storage
 
     })
 
 
 }
 
-beginQuizEl.addEventListener("click", startQuiz);
