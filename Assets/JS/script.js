e2d7ba21c21dcb9b7e131f65611bcd3aa35b134b
FCarter09 <fc_vstate@yahoo.com>
2023-05-29 16:51:17 -0400

    finalPage Update

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,15 @@
 
-
-
 var beginQuizEl = document.querySelector("#start-quiz");
-var time = 60;
+var input 
+var finalScore
+var time = 60; 
+
 
  
 // start quiz on intro page
 var startQuiz = function(){
 
     
-
-    var beginQuizEl = document.querySelector("#start-quiz");
     alert("The quiz has begun, timer has started at 60 seconds!");
     timer();
     questionOne();
@@ -22,22 +21,36 @@ var startQuiz = function(){
 beginQuizEl.addEventListener("click", startQuiz);
 
 // timer function
-var timer = function() {
-
+ var timer = function() {
+   
     var timeRemaining = document.querySelector("#timeRemaining")
 
      // starts timer countdown starting from value of timeLeft
     var countDown = setInterval(function() {
         time--;
         timeRemaining.textContent = time;
-        console.log(time);
+        // console.log(time);
 
         if (time <= 0) {
-            clearInterval(countDown);   // stops at 0
-            // timerElement.textContent = "Make final choice and game will end!";
-        }
+        
+            clearInterval(countDown); // stops at 0
+
+            // alert("Game over! The timer has run out. Please click Ok to save your score.");
+
+            // conclusionPage()
+
+
+
+         } 
+
+
+        
+        
     }, 1000);    //counts in milliseconds. 1000ms = 1second
+
+   
 }
+
 
 // question one function
 var questionOne = function(){
@@ -79,45 +92,73 @@ var questionOne = function(){
 
     // add event listener to buttons
     answerChoiceOne.addEventListener('click', function(event) {
-        
-        alert("Incorrect!");
 
         time -= 20
 
-        questionTwo();
+        alert(" Incorrect! User score is now "  + time + ".");
+
+        questionTwo()
+
+       
     })
 
     answerChoiceTwo.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
 
-        questionTwo();
+        alert(" Incorrect! User score is now "  + time + ".");
+
+        questionTwo()
+
     })
 
     answerChoiceThree.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
 
+        alert(" Incorrect! User score is now "  + time + ".");
 
-        questionTwo();
+        questionTwo()
     })
 
     answerChoiceFour.addEventListener('click', function(event) {
         
         time += 20
 
-        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+        alert(" Correct! You've earned 20 points. User score is now "  + time + ".");
 
         console.log(" User Score is now " + time + ".");
 
         questionTwo();
     })
+    
+    // if time is less or equal to 0, clear elements on questionOne so conclusionPage() can run in the timer()
+    if (time <= 0) {
 
-    // execute next function to start next question
+
+        //delete questionOneEl
+        var questionOneEl = document.querySelector("div");
+        questionOneEl.remove();
+        console.log("removing questionOneEl");
+
+        // remove answer buttons for questionOneEl
+        var answerChoiceOne = document.querySelector("button")
+        answerChoiceOne.remove();
+        console.log("removing answerChoiceOne");
+
+        var answerChoiceTwo = document.querySelector("button")
+        answerChoiceTwo.remove();
+        console.log("removing answerChoiceTwo");  
+
+        var answerChoiceThree = document.querySelector("button")
+        answerChoiceThree.remove();
+        console.log("removing answerChoiceThree"); 
+
+        var answerChoiceFour = document.querySelector("button")
+        answerChoiceFour.remove();
+        console.log("removing answerChoiceFour"); 
+
+    }
 
 }
 
@@ -181,23 +222,21 @@ var questionTwo = function() {
         
     // add event listener to buttons
     answerChoiceOne.addEventListener('click', function(event) {
-       
-        alert("Incorrect!");
 
         time -= 20
-        
 
-        questionThree();
+        alert(" Incorrect! User score is now "  + time + ".");
+        
+        questionThree()
     })
 
     answerChoiceTwo.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
 
+        alert(" Incorrect! User score is now "  + time + ".");
 
-        questionThree();
+        questionThree()
 
     })
 
@@ -205,7 +244,7 @@ var questionTwo = function() {
         
         time += 20
 
-        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+        alert(" Correct! You've earned 20 points. User score is now "  + time + ".");
 
         console.log( " User Score is now " + time + ".");
 
@@ -215,14 +254,49 @@ var questionTwo = function() {
 
     answerChoiceFour.addEventListener('click', function(event) {
         
-        alert("Incorrect!");
 
         time -= 20
 
-        questionThree();
-})
+        alert(" Incorrect! User score is now "  + time + ".");
+
+        questionThree()
+    })
+
+    // if time is less or equal to 0, clear elements on questionTwo so conclusionPage() can run in the timer()
+    if (time <= 0) {
+
+
+        // remove question two element
+        var questionTwoEl = document.querySelector("div");
+        questionTwoEl.remove();
+        console.log("removing questionTwoEl");
+    
+        //remove answer buttons for questionTwoEl
+        var answerChoiceOne = document.querySelector("button")
+        answerChoiceOne.remove();
+        console.log("removing answerChoiceOne");
+    
+        var answerChoiceTwo = document.querySelector("button")
+        answerChoiceTwo.remove();
+        console.log("removing answerChoiceTwo");  
+    
+        var answerChoiceThree = document.querySelector("button")
+        answerChoiceThree.remove();
+        console.log("removing answerChoiceThree"); 
+    
+        var answerChoiceFour = document.querySelector("button")
+        answerChoiceFour.remove();
+        console.log("removing answerChoiceFour");
+        
+        
+    }
+
 
 }
+
+
+
+
 
 // question three function
 var questionThree = function(){
@@ -281,11 +355,12 @@ var questionThree = function(){
 
     answerChoiceOne.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
 
-        conclusionPage();
+        alert(" Incorrect! User score is now "  + time + ".");
+
+
+        conclusionPage()
 
     })
 
@@ -293,9 +368,11 @@ var questionThree = function(){
         
         time += 20
 
-        alert("Correct! You've earned 20 points. User score is now" + time + ".");
+        alert(" Correct! You've earned 20 points. User score is now "  + time + ".");
 
         console.log("User score is now " + time + ".");
+
+        
         
         conclusionPage();
 
@@ -304,32 +381,72 @@ var questionThree = function(){
 
     answerChoiceThree.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
+
+        alert(" Incorrect! User score is now "  + time + ".");
+
         
-        conclusionPage();
+
+        conclusionPage()
 
     })
 
     answerChoiceFour.addEventListener('click', function(event){
 
-        alert("Incorrect!");
-
         time -= 20
+
+        alert(" Incorrect! User score is now "  + time + ".");
+
        
-        conclusionPage();
+       
+        conclusionPage()
 
 
     })
 
+    // if time is less or equal to 0, clear elements on questionThree so conclusionPage() can run in the timer()
+    if (time <= 0) {   
+
+
+        //remove questionThreeEl
+        var questionThreeEl = document.querySelector("div");
+        questionThreeEl.remove();
+        console.log("removing questionTwoEl");
+
+        //remove answer buttons for questionThreeEl
+        var answerChoiceOne = document.querySelector("button")
+        answerChoiceOne.remove();
+        console.log("removing answerChoiceOne");
+
+        var answerChoiceTwo = document.querySelector("button")
+        answerChoiceTwo.remove();
+        console.log("removing answerChoiceTwo");  
+
+        var answerChoiceThree = document.querySelector("button")
+        answerChoiceThree.remove();
+        console.log("removing answerChoiceThree"); 
+
+        var answerChoiceFour = document.querySelector("button")
+        answerChoiceFour.remove();
+        console.log("removing answerChoiceFour"); 
+
+
+    } 
 }
+
+
+
+
 
 // conclusion function
 var conclusionPage = function() {
 
+    setTimeout(timer, 10000);
+
+
     var finalScore = time
 
+    //remove questionThreeEl
     var questionThreeEl = document.querySelector("div");
     questionThreeEl.remove();
     console.log("removing questionTwoEl");
@@ -351,10 +468,11 @@ var conclusionPage = function() {
     answerChoiceFour.remove();
     console.log("removing answerChoiceFour"); 
 
-     //selecting body tag
-     var bodyTag = document.querySelector("body")
-
-     var conclusion = document.createElement("h1");
+    //selecting body tag
+    var bodyTag = document.querySelector("body")
+    
+    // create elements for conclusion page
+    var conclusion = document.createElement("h1");
     conclusion.className = "conclusion";
     conclusion.textContent = "All Done!";
     bodyTag.appendChild(conclusion);
@@ -383,17 +501,139 @@ var conclusionPage = function() {
     // should save final score + initials in local storage. 
     
     console.log('This is working');
-
+    
+    // variable to get value of initialsInput
     var input = document.querySelector(".initialsInput").value
-    // var initialsInput = "Initials"
+    
+
+
+    // get data out of local storage
+    var allUsersData = JSON.parse(localStorage.getItem("finalScores") || "[]");
+
+    // create object with input and final score
+    var userData = {
+         initials: input,
+         score: finalScore
+
+    }
+
+    // add user data to all users data
+    allUsersData.push(userData)
 
     //sets item to local storage
-    localStorage.setItem(input, JSON.stringify(finalScore))
+    localStorage.setItem('finalScores', JSON.stringify(allUsersData))
+
+    finalPage()
+
+    })
+}
 
 
+
+
+
+// final page function
+var finalPage = function() {
+
+    // remove elements from previous page(conclusion page)
+
+    var header = document.querySelector("header")
+    header.remove();
+
+    var conclusion = document.querySelector(".conclusion")
+    conclusion.remove();
+
+    var message = document.querySelector(".message")
+    message.remove();
+
+    var initials = document.querySelector(".initials")
+    initials.remove();
+
+    var initialInput = document.querySelector(".initialsInput")
+    initialInput.remove();
+
+    var submitButton = document.querySelector(".submit-score-button")
+    submitButton.remove()
+
+
+    //selecting body tag
+    var bodyTag = document.querySelector("body")
+
+    // create elements for final page
+    var highScores = document.createElement("h1");
+    highScores.className = "high-scores";
+    highScores.textContent = "High scores";
+    bodyTag.appendChild(highScores);
+
+    // create div container for 'go back' button, 'clear scores' button, and list items
+    var liButtonDiv = document.createElement("div")
+    liButtonDiv.className = "li-button-div"
+    bodyTag.appendChild(liButtonDiv)
+
+    // create go back button
+    var goBackButton = document.createElement("button")
+    goBackButton.className = "go-back-button"
+    goBackButton.textContent = "Go back"
+    liButtonDiv.appendChild(goBackButton)
+
+    // create clear score button
+    var clearScoresButton = document.createElement("button")
+    clearScoresButton.className = "clear-scores-button"
+    clearScoresButton.textContent = "Clear high scores"
+    liButtonDiv.appendChild(clearScoresButton)
+    
+    // pull initials and score from local storage 
+    var allScores = JSON.parse(localStorage.getItem("finalScores") || "[]");
+    console.log('all scores', allScores);
+
+    // sort items in finalScores in descending order by their 'score' value
+    var sortedScores = allScores.sort((a, b) => (a.score - b.score)).reverse()
+    console.log("before:", sortedScores);
+
+    // limit array(list) to just top 5 highest scores
+    sortedScores = sortedScores.slice(0,5)
+    console.log("after:", sortedScores);
+
+    
+
+    // put scores onto page, loop over all sorted scores
+    for (var i = 0; i < sortedScores.length; i++) {
+        
+        // create list items and put inside liButtonDiv
+        var listItem = document.createElement("li")
+        listItem.className = "saved-score-item"
+        listItem.textContent = (JSON.stringify(sortedScores[i])).replace('{"initials":"', '')// removes unneeded characters from sortedScores[i] string
+        .replace('","score":', '-').replace('}', '')// removes unneeded characters from sortedScores[i] string, is connected to line above
+        listItem.setAttribute("type", "1")
+        liButtonDiv.appendChild(listItem)
+
+    }
+
+
+    // add event listener to 'go back' button
+    goBackButton.addEventListener("click", function() {
+            
+        restartQuiz()
 
     })
 
 
+    // add event listener to 'clear scores' button
+    clearScoresButton.addEventListener('click', function() {
+
+        listItem = document.querySelector('li')
+        listItem.remove()
+
+        localStorage.clear()
+
+      
+
+    })
+
 }
+
+
+
+
+
 
